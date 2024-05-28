bea8d2ce2712708a2914dbe72ffb15bac5c83252
Jan <j2000.vdm@gmail.com>
2024-05-28 10:54:21 +0200

    Update to parser (enabling output file specification), readme and added some examples.

--- parser/parse_input.jl
+++ parser/parse_input.jl
@@ -18,17 +18,17 @@ function parse_number(start_index, input)
 end
 
 function parse(input, start_index=0)
-    nodes, edges = "",""
+    nodes, edges, output = "","",""
     parent, index = start_index, start_index
     # parent_stack keeps track of the parent node
     # child_stack keeps track of the last child number of the last depth
     parent_stack, child_stack = Stack{Int64}(), Stack{Int64}() 
     # Initialize the root
     if start_index != 0
-        nodes = nodes * "comp_root($start_index)"
-        nodes = nodes * "\ncomp_node($(start_index), $(input[1]))"
+        nodes = nodes * "comp_root($start_index)."
+        nodes = nodes * "\ncomp_node($(start_index), $(input[1]))."
     else
-        nodes = nodes * "node($(start_index), $(input[1]))"
+        nodes = nodes * "node($(start_index), $(input[1]))."
     end
     # Iterate over the input string
     i = 2
@@ -46,39 +46,43 @@ function parse(input, start_index=0)
             number, i = parse_number(i, input)
             index += 1 # Nr of node / edge
             if start_index != 0
-                nodes = nodes * "\ncomp_node($index, $number)"
+                nodes = nodes * "\ncomp_node($index, $number)."
             else
-                nodes = nodes * "\nnode($index, $number)"
+                nodes = nodes * "\nnode($index, $number)."
             end
             child_nr = pop!(child_stack)
-            edges = edges * "\nedge($parent, $index, $child_nr)"
+            edges = edges * "\nedge($parent, $index, $child_nr)."
             push!(child_stack, child_nr + 1)
         end
         i += 1
     end
     if start_index != 0
-        println("%Compression tree nodes\n" * nodes * "\n%Compression tree edges:" * edges)
+        output = "%Compression tree nodes\n" * nodes * "\n%Compression tree edges:" * edges
     else 
-        println("%Nodes\n" * nodes * "\n\n%Edges:" * edges)
+        output = "%Maint AST\n%Nodes\n" * nodes * "\n%Edges:" * edges
     end
-    return index
+    return index + 1, output
 end
 
-function parse_json(json_path)
+function parse_json(json_path, output_path)
     # Read in the JSON file
     json_content = read(json_path, String)
     json_parsed = JSON.parse(json_content)
     ast = json_parsed["ast"]
     subtrees = json_parsed["subtrees"]
     # Parse the JSON file
-    index = parse(ast) + 1
+    index, output = parse(ast)
     for (i, subtree) in enumerate(subtrees)
-        println("\nSubtree $i")
-        index = parse(subtree, index) + 1
+        index, temp_output = parse(subtree, index)
+        output = output * ("\n\n%Subtree $i\n") * temp_output
+    end
+    # Write the output to a file
+    open(output_path, "w") do f
+        write(f, output)
     end
 end
 
-parse_json(ARGS[1])
+parse_json(ARGS[1], ARGS[2])
 """
 Schema:
 Node(id, grammar_rule) e.g. Node(1, 1)
