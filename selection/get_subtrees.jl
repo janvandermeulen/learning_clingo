ae3936ad484f08160a0e9bf14c585d6692fce868
Pallabi <p.s.p.sarker@student.tudelft.nl>
2024-06-03 23:25:11 +0200

    Working enumeration of all subtrees

--- selection/get_subtrees.jl
+++ selection/get_subtrees.jl
@@ -1,24 +1,53 @@
-using Pkg; Pkg.add("IterTools")
+using HerbSearch, HerbCore, HerbSpecification, HerbInterpret, HerbGrammar
 
-function select_subtrees(tree::AbstractRuleNode)
+function sum_alt(xs::typeof([]))
+    sum = 0	
+    for x in xs
+        sum += x
+    end
+    return sum
+end
+
+function select_subtrees(tree::RuleNode, g::AbstractGrammar)
+    child_subtrees = []
     subtrees = []
+    for child in tree.children
+        subtrees_child = select_subtrees(child, g)
+        push!(child_subtrees, subtrees_child)
+        subtrees = vcat(subtrees_child, subtrees)
+        # print("child subtrees:", [rulenode2expr(sub, g) for sub in subtrees], "\n")
+    end
+    
     for perm in permutations(length(tree.children))
-        subtree = deepcopy(tree)
-        subtree.children = [if perm[i] == 1 tree.children[i] else Hole end for i in 1:length(perm)]
-        push!(subtrees, subtree)
-    end
-    for subtree in subtrees
-        if any(child isa RuleNode for child in subtree.children)
-            subtrees = vcat(subtrees, select_subtrees(subtree))
+        if sum_alt(perm) == 0
+            continue
         end
+        for i in 1:length(perm)
+            subtree = deepcopy(tree)
+            if perm[i] == 1
+                for child_subtree in child_subtrees[i]
+                    # insert!(subtree, HerbGrammar.root_node_loc(subtree.children[i]), child_subtree)
+                    subtree.children[i] = child_subtree
+                end
+            else
+                # path_to_hole = get_path(subtree, subtree.children[i])
+                hole = Hole(get_domain(g, g.bytype[child_types(g, tree)[i]]))
+                # swap_node(subtree, hole, path_to_hole)
+                subtree.children[i] = hole
+                # insert!(subtree, HerbGrammar.root_node_loc(subtree.children[i]), Hole(get_domain(g, g.bytype[child_types(g, tree)[i]])))
+            end
+            push!(subtrees, subtree)
+        end	
     end
+    push!(subtrees, tree)
+    # print([rulenode2expr(sub, g) for sub in subtrees], "\n")
     return subtrees
 end
 
 
 function permutations(n::Int)
     if n == 0
-        return [[]]
+        return []
     else
         smaller_permutations = permutations(n - 1)
         return vcat([vcat(1, perm) for perm in smaller_permutations], [vcat(0, perm) for perm in smaller_permutations])
--- selection/get_subtrees_test.jl
+++ selection/get_subtrees_test.jl
@@ -7,18 +7,13 @@ include("get_subtrees.jl")
 # define our very simple context-free grammar
 # Can add and multiply an input variable x or the integers 1,2.
 g = @csgrammar begin
-    Element = Number # 1
-    Element = Bool # 2
-
     Number = |(1 : 3) # 3-5
     
     Number = Number + Number # 6
-    Bool = Number ≡ Number # 7
-    Number = x # 8
-    
-    Number = Bool ? Number : Number # 9
-    Bool = Bool ? Bool : Bool # 10
 end
 
-random_tree = rand(RuleNode, g, 5)
-subtrees = select_subtrees(random_tree)
+random_tree = rand(RuleNode, g, 2)
+println(rulenode2expr(random_tree, g))
+subtrees = select_subtrees(random_tree, g)
+
+print([rulenode2expr(subtree, g) for subtree in subtrees])
