0485d3bfd6a355844a3071dfdcbf44c0dd4eba24
Pallabi <p.s.p.sarker@student.tudelft.nl>
2024-06-04 10:43:09 +0200

    code cleanup

--- selection/get_subtrees.jl
+++ selection/get_subtrees.jl
@@ -1,13 +1,5 @@
 using HerbSearch, HerbCore, HerbSpecification, HerbInterpret, HerbGrammar
 
-function sum_alt(xs::typeof([]))
-    sum = 0	
-    for x in xs
-        sum += x
-    end
-    return sum
-end
-
 function select_subtrees(tree::RuleNode, g::AbstractGrammar)
     child_subtrees = []
     subtrees_tree_root = []
@@ -15,64 +7,46 @@ function select_subtrees(tree::RuleNode, g::AbstractGrammar)
     for child in tree.children
         (subtrees_child, other_subtrees_child) = select_subtrees(child, g)
         push!(child_subtrees, subtrees_child)
-        other_subtrees = vcat(other_subtrees, subtrees_child)
-        other_subtrees = vcat(other_subtrees, other_subtrees_child)
-        # print("child subtrees:", [rulenode2expr(sub, g) for sub in subtrees], "\n")
+        other_subtrees = vcat(other_subtrees, subtrees_child, other_subtrees_child)
     end
     
-    temp = permutations_helper(length(tree.children))
-    # print(temp, "\n")
-
-    print("permutations: ", temp, "\n")
-    # for every permutation
-    for perm in temp
-        print(perm, "\n")
-        if sum_alt(perm) == length(perm)
-            print("break\n")
+    # for every combination
+    for perm in combinations_helper(length(tree.children))
+        if sum(perm) == length(perm)
             continue
         end
-        print("continue\n")
         subtree_candidates = []
         push!(subtree_candidates, deepcopy(tree)) # copy the tree
         # for every child
-        for i in 1:length(perm)
+        for (i, include) in pairs(perm)
             for candidate in subtree_candidates
-                if perm[i] == 1
-                    for j in 1:length(child_subtrees[i])
+                if include
+                    for (j, child_subtree) in pairs(child_subtrees[i])
                         subtree = j == 1 ? candidate : deepcopy(candidate)
-                        child_subtree = child_subtrees[i][j]
                         subtree.children[i] = child_subtree
-                        # push!(subtree_candidates, subtree)
                     end
                 else
                     hole = Hole(get_domain(g, g.bytype[child_types(g, candidate)[i]]))
                     candidate.children[i] = hole
                 end
-                # print(perm, rulenode2expr(subtree, g), "\n")
             end
         end	
         subtrees_tree_root = vcat(subtree_candidates, subtrees_tree_root)
     end
     push!(subtrees_tree_root, tree)
-    # print("=======\n")
-    # print([rulenode2expr(sub, g) for sub in subtrees], "\n")
     return (subtrees_tree_root, other_subtrees)
 end
 
-function permutations_helper(n::Int)
-    if n == 0
-        return []
-    else
-        return permutations(n)
-    end
+function combinations_helper(n::Int)
+    return n == 0 ? [] : combinations(n)
 end
 
-function permutations(n::Int)
+function combinations(n::Int)
     if n == 0
         return [[]]
-    else
-        smaller_permutations = permutations(n - 1)
-        return vcat([vcat(1, perm) for perm in smaller_permutations], [vcat(0, perm) for perm in smaller_permutations])
     end
+    smaller_combinations = combinations(n - 1)
+    return vcat([vcat(true, perm) for perm in smaller_combinations], 
+        [vcat(false, perm) for perm in smaller_combinations])
 end
 
