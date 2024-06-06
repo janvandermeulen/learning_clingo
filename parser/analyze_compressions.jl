60eff93404d4c54dd5c2606fbe0ad975c87cd495
Rixt <r.hellinga@student.tudelft.nl>
2024-06-06 17:55:54 +0200

    refactored into named tuples

--- parser/analyze_compressions.jl
+++ parser/analyze_compressions.jl
@@ -1,25 +1,38 @@
-
-# Compression_info = (key: subtree ID, value: Tuple([subtree node IDs], compression size, # times the subtree is used as compression))
-Compression_info = Dict{Int64, Tuple{Vector, Int64, Int64}}()
+# c_info = (key: subtree ID, value: Tuple([subtree node IDs], subtree size, # times the subtree is used as compression))
+c_info = Dict{Int64, NamedTuple{(:nodes,:occurences), <:Tuple{Vector,Int64}}}()
 
 # INPUT:
 # d: a dictionary(key: compression_id, value: list of nodes)
 # compressed_AST: ["assign(A, X)", assign(B, Y), ...]
 function analyze_AST_singular(d, compressed_AST)
     for assign in compressed_AST
-        comp_node = parse(Int64, (SubString(assign, 8, 8)))
-        comp_matches = [k for (k,v) in d if comp_node in v][1]
+
+        # parse the compression node id
+        node_id = parse(Int64, (SubString(assign, 8, 8)))
+
+        # find all the compressions that contain that node
+        comp_matches = [C for (C, nodes) in d if node_id in nodes]
+
+        # there should only be one compression with that node id
         @assert length(comp_matches) == 1
-        comp_id = comp_matches[1]
-        if haskey(Compression_info, comp_id)
-            Compression_info[comp_id] = (Compression_info[comp_id][1], Compression_info[comp_id][2], Compression_info[comp_id][3] + 1)
+        C = comp_matches[1]
+
+        # increment the counter if the compression C has been used already
+        if haskey(c_info, C)
+            c_info[C] = (nodes = c_info[C].nodes, occurences = c_info[C].occurences + 1)
+
+        # initialize the counter for the first usage of the compression
         else
-            Compression_info[comp_id] = (d[comp_id], length(d[comp_id]), 1)
+            c_info[C] = (nodes = d[C], occurences = 1)
         end
     end
 
-    for (_,v) in Compression_info
-        @assert mod(v[3], v[2]) == 0
+    for (C, v) in c_info
+        s = length(v.nodes)
+
+        # the number of occurences of a compression must be exactly divisible by its size
+        @assert mod(v.occurences, s) == 0
+        c_info[C] = (nodes = v.nodes, occurences = trunc(Int, v.occurences / s))
     end
 end
 
@@ -27,10 +40,11 @@ end
 ### EXAMPLE
 ### Subtree_dict: key: compression id, value: list of nodes
 Subtree_dict = Dict{Int64, Vector}(1 => [7,8,9], 0 => [2,3,5])
+
 ### compression node with id 2 is assigned to ast node with id 3
 c_ast = ["assign(2, x)", "assign(3, x)", "assign(5, x)", "assign(8, x)", "assign(9, x)", "assign(7, x)"]
 
 analyze_AST_singular(Subtree_dict, c_ast)
 
 println("compression information")
-println(Compression_info)
+println(c_info)
