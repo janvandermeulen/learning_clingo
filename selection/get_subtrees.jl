857dcd73af19b69958f8f4c8e124651909b6a1af
Matteo Bertorotta <matteo.bertorotta@hotmail.com>
2024-06-07 14:32:17 +0200

    fixed an issue in get_subtrees

--- selection/get_subtrees.jl
+++ selection/get_subtrees.jl
@@ -1,20 +1,22 @@
 using HerbSearch, HerbCore, HerbSpecification, HerbInterpret, HerbGrammar
 
 function select_subtrees(tree::RuleNode, g::AbstractGrammar)
+    if length(tree.children) == 0
+        return ([tree], [])
+    end   
     child_subtrees = []
-    subtrees_tree_root = []
-    other_subtrees = []
+    subtrees_tree_root = [] # subtrees with papa node
+    other_subtrees = [] # subtrees without papa node
+
     for child in tree.children
         (subtrees_child, other_subtrees_child) = select_subtrees(child, g)
         push!(child_subtrees, subtrees_child)
         other_subtrees = vcat(other_subtrees, subtrees_child, other_subtrees_child)
     end
-    
+
     # for every combination
-    for perm in combinations_helper(length(tree.children))
-        if sum(perm) == length(perm)
-            continue
-        end
+    for perm in combinations(length(tree.children))
+        
         subtree_candidates = []
         push!(subtree_candidates, deepcopy(tree)) # copy the tree
         # for every child
@@ -24,6 +26,9 @@ function select_subtrees(tree::RuleNode, g::AbstractGrammar)
                     for (j, child_subtree) in pairs(child_subtrees[i])
                         subtree = j == 1 ? candidate : deepcopy(candidate)
                         subtree.children[i] = child_subtree
+                        if j != 1
+                            push!(subtree_candidates, subtree)
+                        end
                     end
                 else
                     hole = Hole(get_domain(g, g.bytype[child_types(g, candidate)[i]]))
@@ -33,13 +38,13 @@ function select_subtrees(tree::RuleNode, g::AbstractGrammar)
         end	
         subtrees_tree_root = vcat(subtree_candidates, subtrees_tree_root)
     end
-    push!(subtrees_tree_root, tree)
+
     return (subtrees_tree_root, other_subtrees)
 end
 
-function combinations_helper(n::Int)
-    return n == 0 ? [] : combinations(n)
-end
+# function combinations_helper(n::Int)
+#     return n == 0 ? [] : combinations(n)
+# end
 
 function combinations(n::Int)
     if n == 0
