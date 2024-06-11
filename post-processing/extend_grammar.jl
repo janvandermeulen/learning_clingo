c83503a5ff7f91353bbb2aa9fba8e4e21b9e25fb
RixtHellinga <r.hellinga@student.tudelft.nl>
2024-06-11 11:01:09 +0200

    adapted analyze compressions to work with more logical dictionaries

---
+++ post-processing/extend_grammar.jl
@@ -0,0 +1,3 @@
+# function that takes a tree in the form of a list of nodes, and converts it to a Herb interpretable tree
+
+# function that takes a grammar and a Herb-tree, and extends the gramar with it
