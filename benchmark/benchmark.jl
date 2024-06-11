bec3da1e4cbe56897d11d46ccdf348ed65ee64d4
Jan <j2000.vdm@gmail.com>
2024-06-11 16:37:01 +0200

    Attempt at parsing to RuleNode from csv text or function.

--- benchmark/benchmark.jl
+++ benchmark/benchmark.jl
@@ -3,6 +3,39 @@ include("../grammar_optimiser/grammar_optimiser.jl")
 using DataFrames; using CSV; using Base;
 using HerbGrammar, HerbSpecification, HerbSearch, HerbInterpret
 
+function create_solutions(grammar) :: Vector{RuleNode}
+    csv = CSV.read(joinpath(dirname(@__FILE__), "inputs", "ast.csv"), DataFrame)
+    functions = csv[:, 2]
+    asts = csv[:, 1]
+    for fun in functions
+        println("function: " * fun)
+        println("grammar:\n" * string(grammar))
+        println(string2rulenode(fun, grammar))
+    end
+end
+
+function string2rulenode(str::String, grammar::AbstractGrammar)
+    temp_expr = Meta.parse(str)
+    expr = Base.remove_linenums!(temp_expr)
+    return expr2rulenode(expr, grammar)
+end
+
+function expr2rulenode(expr::Expr, grammar::AbstractGrammar) :: RuleNode
+    if expr.head == :call
+        # get the rule index
+        rule = findfirst(==(expr), grammar.rules)
+        if isnothing(rule)
+            error("Rule: " * string(rule) * "not found in the grammar")
+        end
+        # create a rulenode
+        rulenode = RuleNode(rule, [])
+        return rulenode
+    elseif expr.head == :block
+        rulenode = RuleNode(3, [expr2rulenode(e, grammar) for e in expr.args])
+    else
+        error("Only call and block expressions are supported")
+    end
+end
 
 
 function get_asts() 
@@ -43,4 +76,12 @@ settings = Dict(
     "output_type" => :Number,
     "max_depth_iterator" => 5
 )
-print(benchmark(settings))
+# print(benchmark(settings))
+g = @cfgrammar begin
+    Number = |(1:2)
+    Number = x
+    Number = Number + Number 
+    Number = Number * Number
+end
+string2rulenode("1+1", g)
+# create_solutions(g)
