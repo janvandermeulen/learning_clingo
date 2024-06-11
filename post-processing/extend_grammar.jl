c3eebc7db97d405d66cd131ba5e88e6d06a60c7c
RoaRos2808 <roanrosema@gmail.com>
2024-06-11 12:04:29 +0200

    extendGrammar function added

--- post-processing/extend_grammar.jl
+++ post-processing/extend_grammar.jl
@@ -1,13 +1,16 @@
+import Pkg; Pkg.add(["HerbGrammar", "HerbSpecification", "HerbSearch", "HerbInterpret"])
+using HerbGrammar, HerbSpecification, HerbSearch, HerbInterpret
+
 # function that takes a tree in the form of a list of nodes, and converts it to a Herb interpretable tree
 # input:
-#### global dictionary d: (key: node_id, value: namedTuple(compressiond_id, parent_id, child_nr, type))
+#### global dictionary d: (key: node_id, value: namedTuple(compression_id, parent_id, child_nr, type))
 #### one compression ID: int
 # output: Herb tree
 function getNodes(d, compression)
     s = Set()
 
     # get the nodes needed for this tree, aka all that belong to the compression
-    for (node_id ,v) in d
+    for (node_id, v) in d
         if v.compression_id == compression
             push!(s, (node_id, v))
         end
@@ -25,4 +28,24 @@ end
 # input: herb tree, standard grammar
 # output: extended grammar
 function extendGrammar(tree, grammar)
+    new_grammar_rule = rulenode2expr(tree, grammar)
+    add_rule!(grammar, :(Number = $new_grammar_rule))
+
+    return grammar
 end
+
+g = @cfgrammar begin
+    Number = |(1:2)
+    Number = x
+    Number = Number + Number 
+    Number = Number * Number
+end
+
+problem = Problem([IOExample(Dict(:x => x), 2x+1) for x ∈ 1:5])
+
+iterator = BFSIterator(g, :Number, max_depth=5)
+solution, flag = synth(problem, iterator)
+
+new = extendGrammar(solution, g)
+
+println(new)
