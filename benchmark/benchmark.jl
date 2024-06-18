3b3ba4afd9f2758838d6655f4d19af508b2adfc1
Jan <j2000.vdm@gmail.com>
2024-06-18 14:31:39 +0200

    Update with more problems samples.

--- benchmark/benchmark.jl
+++ benchmark/benchmark.jl
@@ -11,28 +11,36 @@ function benchmark(g, settings)
     #2. Extend the grammar
     g_extended = grammar_optimiser(asts, g)
     #2a print the new grammar
-    print("PRINTING NEW GRAMMAR")
-    print(string(g_extended))
-    print("=================================")
+    println("PRINTING NEW GRAMMAR")
+    println(string(g_extended))
+    println("=================================")
     #3. Get the problem
-    problem = settings["problem"]
+    # problem = settings["problem"]
     #4. Synthesise the solution using the original grammar
     start_time = time()
-    iterator = BFSIterator(g, settings["output_type"], max_depth=settings["max_depth_iterator"])
-    solution_original, _ = synth(problem, iterator)
+    for problem in settings["problem_set"]
+        iterator = BFSIterator(g, settings["output_type"], max_depth=settings["max_depth_iterator"])
+        solution_original, _ = synth(problem, iterator)
+    end
+    # iterator = BFSIterator(g, settings["output_type"], max_depth=settings["max_depth_iterator"])
+    # solution_original, _ = synth(problem, iterator)
     end_time = time()
 
     time_taken_original = end_time - start_time
     #5. Synthesise the solution using the extended grammar
     start_time = time()
-    iterator = BFSIterator(g_extended, settings["output_type"], max_depth=settings["max_depth_iterator"])
-    solution_extended, _ = synth(problem, iterator)
+    # iterator = BFSIterator(g_extended, settings["output_type"], max_depth=settings["max_depth_iterator"])
+    # solution_extended, _ = synth(problem, iterator)
+    for problem in settings["problem_set"]
+        iterator = BFSIterator(g_extended, settings["output_type"], max_depth=settings["max_depth_iterator"])
+        solution_extended, _ = synth(problem, iterator)
+    end
     end_time = time()
     return Dict(
         "time_taken_original" => time_taken_original, 
         "time_taken_extended" => end_time - start_time,
-        "AST_size_original" => length(solution_original),
-        "AST_size_extended" => length(solution_extended)
+        # "AST_size_original" => length(solution_original),
+        # "AST_size_extended" => length(solution_extended)
     )
 end
   
@@ -40,8 +48,15 @@ settings = Dict(
     "output_type" => :Number,
     "max_depth_iterator" => 5,
     "input_path" => joinpath(dirname(@__FILE__), "inputs", "ast.csv"),
-    "problem" => Problem([IOExample(Dict(:x => x), 2x+1) for x ∈ 1:5])
+    # The set of problems to test efficacy against
+    # "problem_set" => [Problem([IOExample(Dict(:x => x), 2x+1) for x ∈ 1:5]), 
+    #               Problem([IOExample(Dict(:x => x), 3x+5) for x ∈ 1:5]),
+    #               Problem([IOExample(Dict(:x => x), 2+4x) for x ∈ 1:5]),
+    #               Problem([IOExample(Dict(:x => x), 5x+5) for x ∈ 1:5]),
+    #               Problem([IOExample(Dict(:x => x), 2x+2+2x+2) for x ∈ 1:5])]
+    "problem_set" => [Problem([IOExample(Dict(:x => x), 2x+3+2x+3+2x+3+2x) for x ∈ 1:5])]
 )
+
 g = @cfgrammar begin
     Number = |(1:2)
     Number = x
@@ -52,37 +67,20 @@ end
 result = benchmark(g, settings)
 print(string(result))
 
-
-#=
-function benchmark(settings)
-    (asts, g) = get_asts()
-    problem = settings["problem"]
-
-    start_time = time()
-    iterator = BFSIterator(g, settings["output_type"], max_depth=settings["max_depth_iterator"])
-    solution_original, _ = synth(problem, iterator)
-
-    end_time = time()
-    time_taken_original = end_time - start_time
-
-    g = grammar_optimiser(asts, g)
-    start_time = time()
-    solution_extended, _ = synth(problem, BFSIterator(updated_g, settings["output_type"], max_depth=settings["max_depth_iterator"]))
-    end_time = time()
-    time_taken_extended = end_time - start_time
-
-    return Dict(
-        "time_taken_original" => time_taken_original, 
-        "time_taken_extended" => time_taken_extended,
-        "AST_size_original" => length(solution_original),
-        "AST_size_extended" => length(solution_extended)
-    )
-end
-
-settings = Dict(
-    "output_type" => :String,
-    "max_depth_iterator" => 10
-)
-
-format_string_grammars("results.csv")
+#= 
+A set of problems
+problems = []
+functions = []
+push!(problems, Problem([IOExample(Dict(:x => x), 2x+1) for x ∈ 1:5]))
+push!(functions, "2x+1")
+push!(problems, Problem([IOExample(Dict(:x => x), 3x+5) for x ∈ 1:5]))
+push!(problems, Problem([IOExample(Dict(:x => x), 2+4x) for x ∈ 1:5]))
+push!(problems, Problem([IOExample(Dict(:x => x), 5x+5) for x ∈ 1:5]))
+push!(problems, Problem([IOExample(Dict(:x => x), 2x+2+2x+2) for x ∈ 1:5]))
+push!(problems, Problem([IOExample(Dict(:x => x), 4x+3+2x) for x ∈ 1:5]))
+push!(problems, Problem([IOExample(Dict(:x => x), 2x+3+2x) for x ∈ 1:5]))
+push!(problems, Problem([IOExample(Dict(:x => x), 2x+3+2x+3) for x ∈ 1:5]))
+push!(problems, Problem([IOExample(Dict(:x => x), 2x+3+2x+3+2x) for x ∈ 1:5]))
+push!(problems, Problem([IOExample(Dict(:x => x), 2x+3+2x+3+2x+3) for x ∈ 1:5]))
+push!(problems, Problem([IOExample(Dict(:x => x), 2x+3+2x+3+2x+3+2x) for x ∈ 1:5]))
 =#
