c0438a54af1941e15ca5b7db03e2c6dfbf2ef383
Pallabi <p.s.p.sarker@student.tudelft.nl>
2024-06-18 14:28:38 +0200

    herbbenchmark added

--- benchmark/benchmark.jl
+++ benchmark/benchmark.jl
@@ -1,7 +1,6 @@
-import Pkg; Pkg.add(["HerbGrammar", "HerbSpecification", "HerbSearch", "HerbInterpret"])
+import Pkg; Pkg.add(["HerbGrammar", "HerbSpecification", "HerbInterpret"])
 include("../grammar_optimiser/grammar_optimiser.jl")
-include("benchmark_setup.jl")
-# include("../HerbBenchmarks.jl/src/herb_benchmarks.jl")
+include("../HerbBenchmarks.jl/src/data/SyGuS/PBE_SLIA_Track_2019/PBE_SLIA_Track_2019.jl")
 using DataFrames; using CSV; using Base;
 using HerbGrammar, HerbSpecification, HerbSearch, HerbInterpret
 
@@ -31,37 +30,4 @@ function benchmark(g, settings)
     )
 end
 
-
-#=
-function benchmark(settings)
-    (asts, g) = get_asts()
-    problem = settings["problem"]
-
-    start_time = time()
-    iterator = BFSIterator(g, settings["output_type"], max_depth=settings["max_depth_iterator"])
-    solution_original, _ = synth(problem, iterator)
-
-    end_time = time()
-    time_taken_original = end_time - start_time
-
-    g = grammar_optimiser(asts, g)
-    start_time = time()
-    solution_extended, _ = synth(problem, BFSIterator(updated_g, settings["output_type"], max_depth=settings["max_depth_iterator"]))
-    end_time = time()
-    time_taken_extended = end_time - start_time
-
-    return Dict(
-        "time_taken_original" => time_taken_original, 
-        "time_taken_extended" => time_taken_extended,
-        "AST_size_original" => length(solution_original),
-        "AST_size_extended" => length(solution_extended)
-    )
-end
-
-settings = Dict(
-    "output_type" => :String,
-    "max_depth_iterator" => 10
-)
-
 format_string_grammars("results.csv")
-=#
