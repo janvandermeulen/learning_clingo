72171806618be26bc5a1c86bcc8133d7bbfd21db
Jan <j2000.vdm@gmail.com>
2024-06-20 17:02:57 +0200

    Updated the dual bool grammar and added 100 test cases.

--- benchmark/grammars.jl
+++ benchmark/grammars.jl
@@ -9,13 +9,24 @@ single_arith_grammar = @cfgrammar begin
 end
 
 dual_arith_bool_grammar = @cfgrammar begin
-    Number = |(1:2)
-    Number = x
-    Number = y
-    Number = Number + Number 
-    Number = Number - Number
-    Number = Number * Number
-    Number = Number / Number
+    Element = Number # 1
+    Element = Bool # 2
+    # Number logic
+    Number = |(1:2) # 3-4
+    Bool = true
+    Bool = false
+    # Bool = |(true:false) # 5-6
+    Number = x # 7
+    Number = y # 8
+    Number = Number + Number # 9
+    Number = Number - Number # 10
+    Number = Number * Number # 11
+    Number = Number / Number # 12 
+    # Bool logic
+    Bool = Number ≡ Number # 13
+    Bool = Number > Number # 14
+    Number = Bool ? Number : Number # 15
+    Bool = Bool ? Bool : Bool # 16
 end
 
 base_grammar = @cfgrammar begin
@@ -32,9 +43,10 @@ bool_grammar = @csgrammar begin
     Number = |(1 : 3) # 3-5
     
     Number = Number + Number # 6
-    Bool = Number ≡ Number # 7
-    Number = x # 8
+    Number = Number - Number # 7
+    Bool = Number ≡ Number # 8
+    Number = x # 9
     
-    Number = Bool ? Number : Number # 9
-    Bool = Bool ? Bool : Bool # 10
+    Number = Bool ? Number : Number # 10
+    Bool = Bool ? Bool : Bool # 11
 end
