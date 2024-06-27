5ae6e568b7b05d102bdd90701190e54e2df27114
RixtHellinga <r.hellinga@student.tudelft.nl>
2024-06-27 21:31:30 +0200

    created method to bypass json inputs for clingo

--- grammar_optimiser/parse_input.jl
+++ grammar_optimiser/parse_input.jl
@@ -24,6 +24,73 @@ function parse_number(start_index, input)
     return number, i - 1
 end
 
+function parse_tree_to_clingo(input, index, comp = false)
+    """
+    Parses a tree from a string.
+    # Arguments
+    - `input::RuleNode`: the input tree
+    - `global_dict::Dict`: the global dictionary
+    - `start_index::Int64`: the index to start parsing from
+    # Result
+    - `index::Int64`: the index of the last node parsed
+    - `output::String`: the parsed tree
+    """ 
+    nodes, edges = "",""
+
+    type = input.ind
+    current_index = index
+
+    # add the node to the nodes,
+    # for each of the children: add an edge + a recursive call to this method
+    if comp
+        nodes = nodes * "\ncomp_node($(index), $(type))."
+    else 
+        nodes = nodes = nodes * "\nnode($(index), $(type))."
+    end
+
+    child_nr = 0
+
+    for child in input.children
+        index = index + 1
+
+        child_nr = child_nr + 1
+
+        if child isa Hole
+            continue
+        end
+
+        # make an edge
+        edges = edges * "\nedge($current_index, $index, $child_nr)." 
+    
+        # and get the string from the recursive call
+        child_nodes, child_edges, index = parse_tree_to_clingo(child, index, comp)
+        # and add it to the nodes and edges
+        nodes = nodes * child_nodes
+        edges = edges * child_edges
+    end   
+
+    return nodes, edges, index
+end
+
+
+function trees_to_clingo(ast, subtrees, i)
+
+    index::Int64 = 1
+
+    # first make the AST
+    nodes, edges, index = parse_tree_to_clingo(ast, index)
+    output = "%Main AST\n%Nodes" * nodes * "\n%Edges:" * edges
+
+    # then make all the compressions
+    for (i, s) in enumerate(subtrees)
+        output = output * ("\n\n%Subtree $i\n") * "%Compression tree nodes\n" * "comp_root($(index+1))."
+        nodes, edges, index = parse_tree_to_clingo(s, index + 1, true)
+        output =  output * nodes * "\n%Compression tree edges:" * edges
+    end
+
+    #TODO: make the global dictionary with rulenodes
+end
+
 function parse_tree(input, global_dict=nothing, start_index=0)
     """
     Parses a tree from a string.
