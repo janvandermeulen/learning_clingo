a7257f153eb1c0483c210c8bb3a494812daa4f05
RixtHellinga <r.hellinga@student.tudelft.nl>
2024-06-27 19:46:40 +0200

    removed more redundant code and comments

--- grammar_optimiser/analyze_compressions.jl
+++ grammar_optimiser/analyze_compressions.jl
@@ -2,18 +2,6 @@
 
 Base.isequal(k1::RuleNode, k2::RuleNode) = compare(k1, k2)
 
-
-#TODO: With generate_stats, immediately put in the rulenode as key, then we don't have to convert it later on.
-# Also, we cannot put it earlier as RuleNode in the globaldict, as there it's per node, not per tree
-# But do we then even need the global dictionary? Can we factor it out?
-# IF we create the rulenodes immediately from the parser, we wouldn't need the global dictionary.
-# But we can't do that because we don't have complete compressions yet. Yes we do actually?
-# But how will we match assign-statements with a dictionary where the keys are RuleNodes, instead of node id's
-# redesign the dictionary: key => RuleNode, value => list of node ID's (first one is root/id), size (implicit), occurences
-# And then how do we do the generate_tree_from_compression thing without the global dict? We can already make the dict when enumerating subtrees?
-# we can almost, we have the size, occurences (=0), but not a list of node ids, especially since the nodes dont have any id yet
-# so we'd still have to do that in the parser where ids are assigned
-
 function generate_stats(d, compressed_AST)
     """
     Compression Analysis. Analyzes 1 AST to see how many times each compression was used.
--- grammar_optimiser/grammar_optimiser.jl
+++ grammar_optimiser/grammar_optimiser.jl
@@ -6,14 +6,14 @@ include("parse_output.jl")
 include("analyze_compressions.jl")
 include("extend_grammar.jl")
 
-function run_command(command)
-    """
-    Run a command in the shell.
-    # Arguments
-    - `command::Cmd`: the command to run
-    """
-    run(`$command`)
-end
+# function run_command(command)
+#     """
+#     Run a command in the shell.
+#     # Arguments
+#     - `command::Cmd`: the command to run
+#     """
+#     run(`$command`)
+# end
 
 function grammar_optimiser(trees::Vector{RuleNode}, grammar::AbstractGrammar, subtree_selection_strategy::Int, f_best::Float64)
     """
@@ -27,44 +27,36 @@ function grammar_optimiser(trees::Vector{RuleNode}, grammar::AbstractGrammar, su
     - `new_grammar::AbstractGrammar`: the optimised grammar
     """
     dir_path = dirname(@__FILE__)     
+
+    # 1. Enumerate subtrees
     start_time = time()
-    print("Stage 1: Select subtrees\n")     # 1a. Select subtrees 
-    subtree_set = Vector{Any}()
+    print("Stage 1: Select subtrees\n")      
+    subtree_sets = Vector{Vector{Any}}()
     for tree in trees
         (subtrees_root, other_subtrees) = enumerate_subtrees(tree, grammar)
+        #TODO: why aren't we getting simple subtrees on a an AST of size 3?
         subtrees = vcat(subtrees_root, other_subtrees)
         subtrees = filter(subtree -> selection_criteria(tree, subtree), subtrees) #remove subtrees size 1 and treesize
-        subtree_set = vcat(subtree_set, subtrees)
+        push!(subtree_sets, subtrees)
     end
-
-    #TODO: why do we have one subtree set for everything?
-    subtree_set = unique(subtree_set)
-
     print("Time for stage 1: " * string(time() - start_time) * "\n"); start_time = time()
-    
-    #TODO: get the RuleNode dict from this subtree set
-    # Base.length: Return the number of nodes in the tree rooted at root. Holes don't count.
-    # occurences = 0
-    # BUT, I don't have id's yet here...
 
+    # 2. Parse subtrees to clingo input
+    print("Stage 2: parse subtrees to clingo input\n")
 
-    print("Stage 2: parse subtrees to json\n")
-    # 2. Parse subtrees to json
-
-    for (id, tree) in enumerate(trees)
-        parse_subtrees_to_json(subtree_set, tree, id)
-    end
     global_dicts = []
-    for i in 1:length(trees)
-        input_location = joinpath(dir_path, "inputs", "parser_input$(i).json")
-        output_location = joinpath(dir_path, "clingo_inputs", "model_input$(i).lp")
+    for (id, tree) in enumerate(trees)
+        parse_subtrees_to_json(subtree_sets[id], tree, id)
+        input_location = joinpath(dir_path, "inputs", "parser_input$(id).json")
+        output_location = joinpath(dir_path, "clingo_inputs", "model_input$(id).lp")
         global_dict = parse_json(input_location, output_location)
         push!(global_dicts, global_dict)
     end
     print("Time for stage 2 : " * string(time() - start_time) * "\n"); start_time = time()
-    print("Stage 3: call clingo\n")
+
 
     # 3. Call clingo 
+    print("Stage 3: call clingo\n")
     model_location = joinpath(dir_path, "model.lp")
     for i in 1:length(trees)
         input_location = joinpath(dir_path, "clingo_inputs", "model_input$(i).lp")
@@ -79,14 +71,18 @@ function grammar_optimiser(trees::Vector{RuleNode}, grammar::AbstractGrammar, su
         end
     end
     print("Time for stage 3 : " * string(time() - start_time) * "\n"); start_time = time()
-    print("Stage 4: Parse clingo output to json\n")     # 4. Parse clingo output to json
+
+    # 4. Parse clingo output to json
+    print("Stage 4: Parse clingo output to json\n")
     best_values = []
     for i in 1:length(trees)
         input_location = joinpath(dir_path, "outputs", "clingo_output$(i).json")
         push!(best_values, read_json(input_location))
     end
+    print("Time for stage 4 : " * string(time() - start_time) * "\n"); start_time = time()
 
-    print("Stage 5: Analyze subtrees\n") # 5. Analyse clingo output
+    # 5. Analyse clingo output
+    print("Stage 5: Analyze subtrees\n") 
 
     all_stats = Vector{Dict{RuleNode, NamedTuple{(:size,:occurences), <:Tuple{Int64,Int64}}}}()
 
@@ -108,6 +104,8 @@ function grammar_optimiser(trees::Vector{RuleNode}, grammar::AbstractGrammar, su
     for b in best_compressions
         new_grammar = extendGrammar(b, new_grammar)
     end
+
+    print("Time for stage 5 : " * string(time() - start_time) * "\n"); start_time = time()
 
     return new_grammar
 
